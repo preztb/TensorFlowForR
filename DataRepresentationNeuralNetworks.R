61f24ba0c1da1c27034e85e4ef86f129d22398fb
Tyler Beck <tylerprojects164@gmail.com>
2024-06-06 12:52:26 -0400

    Lunch Break 6-6-24

--- DataRepresentationNeuralNetworks.R
+++ DataRepresentationNeuralNetworks.R
@@ -29,7 +29,7 @@ length(dim(x))
 mnist <- dataset_mnist()
 train_images <- mnist$train$x
 train_labels <- mnist$train$y
-test_labels <- mnist$test$x
+test_images <- mnist$test$x
 test_labels <- mnist$test$y
 
 #2.2.5
@@ -42,8 +42,28 @@ typeof(train_images)
 
 #2.8 displaying the fifth digit
 
-digit <- train_images[5, ]
+digit <- train_images[5, , ]
 plot(as.raster(abs(255 - digit), max = 255))
 
 train_labels[5]
+
+#2.2.6 Manulaping Tensors in R
+
+my_slice <- train_images[10:99, , ]
+dim(my_slice)
+
+my_slice <- train_images[, 15:28, 15:28]
+dim(my_slice)
+
+#1st batch of data - split into batches of 128
+batch <- train_images[1:128, , ]
+
+#2nd batch of data
+batch <- train_images[129:256, , ]
+
+#nth batch of data
+n <- 3
+batch <- train_images[seq(to = 128 * n, length.out = 128), , ]
+
+#2.2.8 real applications of 
 
